939186e6345c9ba4d124f231507a7f2e9d193bf1
nicholas kebut <nicholas.kebut@thejitu.com>
2022-10-16 10:57:15 +0300

    Added form validation

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const title = document.getElementById('tTitle');
+const desc = document.getElementById('tDescription');
+const date = document.getElementById('tDueDate');
+const titleErr = document.getElementById("titlemsg");
+const imgtitleErr = document.getElementById("imgtitle");
+const discMsg = document.getElementById("titledesc");
+const discImg = document.getElementById("imgdesc");
+const dateDesc = document.getElementById("titledate");
+const dateImg = document.getElementById("imgdate");
+const form = document.getElementById('form');
+const submit = document.getElementById('submit');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let Titilevalid = false;
+    let Descvalid = false;
+    let Datevalid = false;
+
+    if (title.value === '' || title.value == null) {
+        titleErr.style.display = 'block';
+        titleErr.innerHTML = "Must have a title";
+        imgtitleErr.src = '../images/cancel.svg';
+        Titilevalid = false;
+    }else{
+        imgtitleErr.src = '../images/tick.svg';
+    }
+    if (desc.value === '' || desc.value == null) {
+        discMsg.style.display = 'block';
+        discMsg.innerHTML = "Must have a description";
+        discImg.src = '../images/cancel.svg';
+        Descvalid = false;
+    }else{
+        discImg.src = '../images/tick.svg';
+    }
+
+    if (date.value) {
+        Datevalid = true;
+        dateImg.src = '../images/tick.svg';
+    } else {
+
+        Datevalid = false;
+
+        dateDesc.style.display = 'block';
+        dateDesc.innerHTML = "Must have a date"
+        dateImg.src = '../images/cancel.svg';
+    }
+
+    if (Titilevalid && Descvalid && Datevalid) {
+
+        imgtitleErr.src = '../images/tick.svg';
+        discImg.src = '../images/tick.svg';
+        dateImg.src = '../images/tick.svg';
+
+    } else {
+
+    }
+});
+
